ae8e43a8e4e00f2a166b84e8701cd734f7be4bad
Antonio Francisco de Araújo Neto <antfcone@gmail.com>
2022-07-14 19:46:53 -0300

    updating-project

--- src/contexts/menuContext.js
+++ src/contexts/menuContext.js
@@ -1,20 +1,20 @@
 import React, { createContext, useState, useContext } from "react";
 const MenuContext = createContext()
 
-export const useMenuContext = ()=>{
+export const useMenuContext = () => {
     return useContext(MenuContext);
 }
-export const AppMenuContext = ({children}) => {
+export const AppMenuContext = ({ children }) => {
     const [isMenuToggled, setToggleMenu] = useState(true)
-    const toggleMenu = ()=>{
-        isMenuToggled ? setToggleMenu(false): setToggleMenu(true)
+    const toggleMenu = () => {
+        isMenuToggled ? setToggleMenu(false) : setToggleMenu(true)
     }
-    return(
-        <MenuContext.Provider value={{isMenuToggled, toggleMenu}}>
-            <div style={{position:"relative", width:"100%"}}>
+    return (
+        <MenuContext.Provider value={{ isMenuToggled, toggleMenu }}>
+            <div style={{ position: "relative", width: "100%" }}>
                 {children}
             </div>
-            
+
         </MenuContext.Provider>
     )
 }
